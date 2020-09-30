33a83cd78a92a6c6d6aaf14163dd35f5053675fc
ilorenzov <ilorenzovelez.guadalupe@alumnado.fundacionloyola.net>
2020-09-30 13:53:22 +0200

    Proyecto JPA facturas sin spring security

---
+++ spring-boot-data-jpa/src/main/java/com/example/springbootdatajpa/app/controllers/FacturaController.java
@@ -0,0 +1,98 @@
+package com.example.springbootdatajpa.app.controllers;
+
+import com.example.springbootdatajpa.app.models.entity.Cliente;
+import com.example.springbootdatajpa.app.models.entity.Factura;
+import com.example.springbootdatajpa.app.models.entity.ItemFactura;
+import com.example.springbootdatajpa.app.models.entity.Producto;
+import com.example.springbootdatajpa.app.models.service.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("factura")
+public class FacturaController {
+    @Autowired
+    private IClienteService clienteService;
+
+    @GetMapping("/form/{clienteId}")
+    public String crear(@PathVariable(value = "clienteId") Long clienteId, Map<String, Object> model, RedirectAttributes flash) {
+        Cliente cliente = clienteService.findOne(clienteId);
+        if (cliente == null) {
+            flash.addFlashAttribute("error", "El cliente no existe");
+            return "redirect:/listar";
+        }
+        Factura factura = new Factura();
+        factura.setCliente(cliente);
+        model.put("factura", factura);
+        model.put("titulo", "Crear Factura");
+
+        return "factura/form";
+    }
+
+    @GetMapping(value = "/cargar-productos/{term}", produces = {"application/json"})
+    public @ResponseBody
+    List<Producto> cargarProductos(@PathVariable String term) { //ResponseBody suprime el cargar una vista de tymeleaf, en vez de eso va a tomar el resultado convertido a json y lo va a registrar dentro del body de la respuesta
+        return clienteService.findByNombre(term);
+    }
+
+    @PostMapping("/form")
+    public String guardar(@Valid Factura factura, BindingResult result, Model model, @RequestParam(name = "item_id[]", required = false) Long[] itemId, @RequestParam(name = "cantidad[]", required = false) Integer[] cantidad,
+                          RedirectAttributes flash, SessionStatus status) {
+        if (result.hasErrors()) {
+            model.addAttribute("titulo", "Crear factura");
+            return "factura/form";
+        }
+        if (itemId == null || itemId.length == 0) {
+            model.addAttribute("titulo", "Crear factura");
+            model.addAttribute("error", "Error:La factura no puede no tener lineas");
+            return "factura/form";
+        }
+        for (int i = 0; i < itemId.length; i++) {
+            Producto producto = clienteService.findProductoById(itemId[i]);
+            ItemFactura linea = new ItemFactura();
+            linea.setCantidad(cantidad[i]);
+            linea.setProducto(producto);
+            factura.addItemFactura(linea);
+        }
+        clienteService.saveFactura(factura);
+        status.setComplete();
+        flash.addFlashAttribute("success", "Factura creada con exito!");
+        return "redirect:/ver/" + factura.getCliente().getId();
+    }
+    @GetMapping(value = "/ver/{id}")
+    public String ver(@PathVariable(value = "id") Long id, Model model, RedirectAttributes flash) {
+        Factura factura = clienteService.fectchByIdWithClienteWithItemFacturaWithProducto(id);       //findFacturaById(id);
+        if (factura == null) {
+            flash.addFlashAttribute("error", "La factura no existe en la base de datos");
+            return "redirect:/listar";
+        }
+        model.addAttribute("factura", factura);
+        model.addAttribute("titulo", "Factura:".concat(factura.getDescripcion()));
+        return "factura/ver";
+
+    }
+
+    @GetMapping(value = "/eliminar/{id}")
+    public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
+        Factura factura = clienteService.findFacturaById(id);
+        if (factura != null) {
+            clienteService.deleteFactura(id);
+            flash.addFlashAttribute("success", "Factura eliminada con exito");
+            return "redirect:/ver/" + factura.getCliente().getId();
+        }
+        flash.addFlashAttribute("error", "La factura no existe en la base de datos, no se pudo eliminar");
+        return "redirect:/listar";
+    }
+
+
+}
---
+++ spring-boot-data-jpa/src/main/java/com/example/springbootdatajpa/app/models/entity/Cliente.java
@@ -0,0 +1,118 @@
+package com.example.springbootdatajpa.app.models.entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+    public Cliente() {
+        facturas = new ArrayList<Factura>();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty //Strings
+    private String nombre;
+    @NotEmpty //Strings
+    private String apellido;
+    @NotEmpty //Strings
+    @Email
+    private String email;
+
+    @NotNull
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE) //
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date createAt;
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    //un cliente puede tener muchas facturas //mapped by es la clave foranea
+    private List<Factura> facturas;
+    private String foto;
+
+    @PrePersist //Se llama al metodo antes de insertar el registro
+    public void prePersist() {
+        createAt = new Date();
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public List<Factura> getFacturas() {
+        return facturas;
+    }
+
+    public void setFacturas(List<Factura> facturas) {
+        this.facturas = facturas;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public void addFactura(Factura factura) {
+        facturas.add(factura);
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido;
+    }
+}
